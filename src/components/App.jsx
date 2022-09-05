373afc30ed3fc0ffc4e7421abbbba72b503e908c
Diego <davc93@gmail.com>
2022-09-05 13:13:15 -0400

    se soluciona el linter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import '../styles/components/app.css';
 
 const hola = 'hola';
-function App() {return <h1>Hola mundo</h1>;
-
-
+function App() {
+  return <h1>Hola mundo</h1>;
 }
 
 export default App;
